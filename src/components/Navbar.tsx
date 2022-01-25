25a4443fead90714c7577ad5dcf5cebe81cc1aff
Gcottin <g.cottin@moderlab.com>
2022-01-25 17:17:48 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const Container = styled("div")`
+    width: 100vw;
+    height: auto;
+    background-color: #af48ca;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    position: relative;
+`
+
+const Title = styled("h1")`
+    margin: 0;
+    white-space: nowrap;
+    font-family: 'Bungee', cursive;
+`
+
+const Link = styled("a")`
+    font-size: 1.5rem;
+    margin: 1rem;
+    cursor: pointer;
+    color: initial;
+    text-decoration: none;
+
+    position: absolute;
+    right: 5%;
+
+    @media (max-width: 700px) {
+        position: initial;
+    }
+`
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Title>
+                Titan Dogs
+            </Title>
+            <Link href="#roadmap">
+                Roadmap
+            </Link>
+        </Container>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Roadmap.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Container = styled("div")`
+    width: 100vw;
+    height: auto;
+    margin-top: 100px;
+    padding: 20px;
+    background-color: white;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const ListItem = styled("li")`
+    margin: 10px;
+`
+
+const Roadmap = () => {
+    return (
+        <Container>
+            <h1>Roadmap</h1>
+            <h1>Q1 2022</h1>
+            <ul style={{padding: "0 20%"}}>
+                <ListItem>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusantium eaque assumenda officia ad libero corrupti, maiores error mollitia, debitis, maxime atque totam quos velit esse. Saepe dolor dolores labore.
+                </ListItem>
+                <ListItem>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. At accusantium eaque assumenda officia ad libero corrupti, maiores error mollitia, debitis, maxime atque totam quos velit esse. Saepe dolor dolores labore.
+                </ListItem>
+
+            </ul>
+        </Container>
+    )
+}
+
+export default Roadmap;
